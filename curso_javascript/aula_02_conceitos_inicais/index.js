eb639f64a87844110226f43843bc1e5d2ecb9566
matcbv <matheuscbv23@gmail.com>
2024-10-16 22:59:35 -0300

    Finalizacao do exercicio da aula 2 do curso de js

--- curso_javascript/aula_02_conceitos_inicais/index.js
+++ curso_javascript/aula_02_conceitos_inicais/index.js
@@ -1,26 +1,46 @@
+const form = document.querySelector('form');
+const spanList = document.querySelectorAll("span");
+const userInfo = document.querySelector(".user-info");
+const days = document.querySelector(".days");
+const birthInput = document.querySelector('.birth-input')
 
-const form = document.querySelector('form')
-const liList = document.querySelectorAll("li")
-const userInfo = document.querySelector(".user-info")
-const days = document.querySelector(".days")
+const currentDate = new Date();
+const miliToDays = 1000 * 60 * 60 * 24;
 
-const date = new Date()
+birthInput.addEventListener('input', (event) => {
+    const data = event.data
+    if(isNaN(parseInt(data))){
+        birthInput.value = birthInput.value.slice(0, -1)
+    }
+    if (Array.from(birthInput.value).length === 2 || Array.from(birthInput.value).length === 5){
+        console.log('passei')
+        birthInput.value += '/'
+    }
+})
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const element = e.target
-    valuesList = getData(element)
-    liList[0].innerHTML += valuesList[0] + " " + valuesList[1]
-    liList[1].innerHTML += date.getFullYear() - Number(valuesList[2].slice(-4))
-    userInfo.style.display = "block"
-    form.submit
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const valuesList = getData(event.target);
+    const birthdayDate = getBirthdayDate(valuesList[2]);
+
+    spanList[0].innerHTML = valuesList[0] + " " + valuesList[1];
+    spanList[1].innerHTML = currentDate.getFullYear() - parseInt(valuesList[2].slice(-4));
+    spanList[2].innerHTML = Math.floor((birthdayDate.getTime() - currentDate.getTime()) / miliToDays);
+
+    userInfo.style.display = "block";
+    form.submit;
 })
 
 function getData(element) {
-    const inputValues = []
-    const inputs = element.querySelectorAll("input")
+    const inputValues = [];
+    const inputs = element.querySelectorAll("input");
     for (input of inputs){
-        inputValues.push(input.value)
+        inputValues.push(input.value);
     }
-    return inputValues
+    return inputValues;
+}
+
+function getBirthdayDate(dateStr){
+    const nextYear = String(parseInt(currentDate.getFullYear()) + 1);
+    return new Date(nextYear + "-" + dateStr.slice(3, 5) + "-" + dateStr.slice(0, 2));
 }
