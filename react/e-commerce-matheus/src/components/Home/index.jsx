5b804b0b3d4e2916b1a01229c42a42ff757c0d85
matcbv <matheuscbv23@gmail.com>
2025-02-17 23:56:08 -0300

    Mudanca do nome do repositorio

---
+++ react/e-commerce-matheus/src/components/Home/index.jsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+export function Home(){
+    return (
+        <div>
+            <h1>Página Inicial</h1>
+        </div>
+    );
+};
---
+++ react/e-commerce-matheus/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// 
+import './index.css';
+
+// Components:
+import { Home } from './components/Home';
+import { NavBar } from './components/NavBar';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={Home}/>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+);
